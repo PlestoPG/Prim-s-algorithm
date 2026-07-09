5106613d8cffa0335d3101c01bd85f119ce467b7
PlestoPG <rogaliki.05@mail.ru>
2026-07-09 18:43:58 +0300

    drag&drop reorganization

--- src/ui/Application.java
+++ src/ui/Application.java
@@ -3,12 +3,6 @@ package ui;
 import model.Graph;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.Transferable;
-import java.io.File;
-import java.nio.file.Files;
-import java.util.List;
-
 
 public class Application extends JFrame {
     protected final JLabel statusLabel = new JLabel();
@@ -30,38 +24,10 @@ public class Application extends JFrame {
         // Панель снизу с ползунком и кнопкой "о разработчиках"
         add(new BottomPanel(this), BorderLayout.SOUTH);
 
+        // Обработка переноса файла или текста в окно приложения
+        setTransferHandler(new DragnDropHandler(this));
+
         setVisible(true);
-
-        setTransferHandler(new TransferHandler() {
-            @Override
-            public boolean canImport(TransferSupport support) {
-                return support.isDataFlavorSupported(DataFlavor.javaFileListFlavor) ||
-                        support.isDataFlavorSupported(DataFlavor.stringFlavor);
-            }
-
-            @Override
-            public boolean importData(TransferSupport support) {
-                try {
-                    String text;
-                    Transferable transferable = support.getTransferable();
-                    if (transferable.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
-                        List<?> files = (List<?>) transferable.getTransferData(DataFlavor.javaFileListFlavor);
-                        File file = (File) files.getFirst();
-                        text = Files.readString(file.toPath());
-                    } else {
-                        text = (String) transferable.getTransferData(DataFlavor.stringFlavor);
-                    }
-                    // GraphParser parser = new GraphParser();
-                    // prototype.graph = parser.parse(string);
-                    setStatus("Граф загружен");
-                    repaint();
-                    return true;
-                } catch (Exception ex) {
-                    setStatus("Не удалось загрузить граф");
-                    return false;
-                }
-            }
-        });
     }
 
     public void setStatus(String status) {
---
+++ src/ui/DragnDropHandler.java
@@ -0,0 +1,45 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.io.File;
+import java.nio.file.Files;
+import java.util.List;
+
+public class DragnDropHandler extends TransferHandler {
+    Application application;
+
+    @Override
+    public boolean canImport(TransferSupport support) {
+        return support.isDataFlavorSupported(DataFlavor.javaFileListFlavor) ||
+                support.isDataFlavorSupported(DataFlavor.stringFlavor);
+    }
+
+    @Override
+    public boolean importData(TransferSupport support) {
+        try {
+            String text;
+            Transferable transferable = support.getTransferable();
+            if (transferable.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+                List<?> files = (List<?>) transferable.getTransferData(DataFlavor.javaFileListFlavor);
+                File file = (File) files.getFirst();
+                text = Files.readString(file.toPath());
+            } else {
+                text = (String) transferable.getTransferData(DataFlavor.stringFlavor);
+            }
+            // GraphParser parser = new GraphParser();
+            // prototype.graph = parser.parse(string);
+            application.setStatus("Граф загружен");
+            application.repaint();
+            return true;
+        } catch (Exception ex) {
+            application.setStatus("Не удалось загрузить граф");
+            return false;
+        }
+    }
+
+    DragnDropHandler(Application application) {
+        this.application = application;
+    }
+}
