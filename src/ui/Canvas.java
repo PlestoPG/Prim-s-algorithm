295bab3e078cca081b5e365a61d32e6b862db2a5
PlestoPG <rogaliki.05@mail.ru>
2026-07-09 22:40:20 +0300

    coloring edges and vertices

--- src/ui/Canvas.java
+++ src/ui/Canvas.java
@@ -15,24 +15,37 @@ public class Canvas extends JPanel {
         super.paintComponent(g);
 
         for (Edge edge : application.graph.getEdges()) {
-            g.setColor(Color.BLUE);
+            Color color;
+            switch (edge.getState()) {
+                case IN_MST ->  color = Color.GREEN;
+                case CONSIDERED -> color = Color.CYAN;
+                default -> color = Color.BLUE;
+            }
+
+            g.setColor(color);
             g.drawLine(edge.getV1().getX(), edge.getV1().getY(), edge.getV2().getX(), edge.getV2().getY());
-            g.setColor(Color.BLACK);
 
             int midX = (edge.getV1().getX() + edge.getV2().getX()) / 2;
             int midY = (edge.getV1().getY() + edge.getV2().getY()) / 2;
 
-            g.drawString(edge.getWeight() + "", midX, midY);
+            g.setColor(Color.BLACK);
+            g.drawString(edge.getWeight() + "", midX - 4 * (edge.getWeight() + "").length(), midY);
         }
 
         if (controller.selectedVertex != null && controller.mousePosition != null) {
             g.setColor(Color.BLUE);
             g.drawLine(controller.selectedVertex.getX(), controller.selectedVertex.getY(), controller.mousePosition.x, controller.mousePosition.y);
-            g.setColor(Color.WHITE);
         }
 
         for (Vertex vertex : application.graph.getVertices()) {
-            g.setColor(Color.BLUE);
+            Color color;
+            switch (vertex.getState()) {
+                case IN_MST ->  color = Color.GREEN;
+                case ACTIVE -> color = Color.RED;
+                default -> color = Color.BLUE;
+            }
+
+            g.setColor(color);
             g.fillOval(vertex.getX() - 15, vertex.getY() - 15, 30, 30);
             g.setColor(Color.WHITE);
             g.drawString(vertex.getName(), vertex.getX() - 4 * vertex.getName().length(), vertex.getY() + 5);
