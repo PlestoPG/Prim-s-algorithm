4ef2174360c13b357d3d0e59ead8f6a223689044
PlestoPG <rogaliki.05@mail.ru>
2026-07-10 16:09:03 +0300

    Added pictures scaling

--- src/ui/AboutDevsWindow.java
+++ src/ui/AboutDevsWindow.java
@@ -5,31 +5,58 @@ import java.awt.*;
 import java.net.URL;
 
 public class AboutDevsWindow extends JPanel {
-    AboutDevsWindow() {
+    int width;
+    int height;
+    int min;
+
+    private JPanel appendMember(String path, String name) {
+        JPanel member = new JPanel(new BorderLayout());
+        URL imageURL = getClass().getResource(path);
+        if (imageURL != null) {
+            ImageIcon imageIcon = new ImageIcon(imageURL);
+            if (!(imageIcon.getIconWidth() <= min && imageIcon.getIconHeight() <= min)) {
+                double scale = Math.min(
+                        (double) min / imageIcon.getIconWidth(),
+                        (double) min / imageIcon.getIconHeight()
+                );
+                int newWidth = (int) (imageIcon.getIconWidth() * scale);
+                int newHeight = (int) (imageIcon.getIconHeight() * scale);
+
+                Image image = imageIcon.getImage().getScaledInstance(
+                        newWidth,
+                        newHeight,
+                        Image.SCALE_SMOOTH
+                );
+                member.add(new JLabel(new ImageIcon(image)), BorderLayout.NORTH);
+            } else {
+                JLabel image = new JLabel(imageIcon);
+                member.add(image, BorderLayout.NORTH);
+            }
+        }
+        member.add(new JLabel(name, SwingConstants.CENTER), BorderLayout.CENTER);
+        add(member);
+        return member;
+    }
+
+    AboutDevsWindow(Application application) {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        URL photoURL;
 
-        add(new JLabel("Бригада №8 (5)"));
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice gd = ge.getDefaultScreenDevice();
+        Rectangle bounds = gd.getDefaultConfiguration().getBounds();
+        Insets insets = Toolkit.getDefaultToolkit().getScreenInsets(
+                gd.getDefaultConfiguration());
+        this.width = bounds.width - insets.left - insets.right;
+        this.height = bounds.height - insets.top - insets.bottom;
+        this.min = Math.min(bounds.width, bounds.height) - 100;
 
-        JPanel member1 = new JPanel(new BorderLayout());
-        photoURL = getClass().getResource("/media/Kirill.jpg");
-        if (photoURL != null)
-            member1.add(new JLabel(new ImageIcon(photoURL)), BorderLayout.NORTH);
-        member1.add(new JLabel("Рогачевский Кирилл Андреевич 4344"), BorderLayout.CENTER);
-        add(member1);
+        add(new JLabel("Бригада №5"));
+        add(appendMember("/media/Kirill.jpg", "Рогачевский Кирилл Андреевич 4344"));
+        add(appendMember("/media/Marina.jpg", "Захарова Марина Игоревна 4344"));
+        add(appendMember("/media/Amirkhan.jpg", "Кяримов Амирхан Икрам Оглы 4381"));
 
-        JPanel member2 = new JPanel(new BorderLayout());
-        photoURL = getClass().getResource("/media/Marina.jpg");
-        if (photoURL != null)
-            member2.add(new JLabel(new ImageIcon(photoURL)), BorderLayout.NORTH);
-        member2.add(new JLabel("Захарова Марина Игоревна 4344"), BorderLayout.CENTER);
-        add(member2);
-
-        JPanel member3 = new JPanel(new BorderLayout());
-        photoURL = getClass().getResource("/media/Amirkhan.jpg");
-        if (photoURL != null)
-            member3.add(new JLabel(new ImageIcon(photoURL)), BorderLayout.NORTH);
-        member3.add(new JLabel("Кяримов Амирхан Икрам Оглы 4381"), BorderLayout.CENTER);
-        add(member3);
+        JScrollPane scrollPane = new JScrollPane(this);
+        scrollPane.setPreferredSize(new Dimension(min + 100, height - 100));
+        JOptionPane.showMessageDialog(application, scrollPane, "О разработчиках", JOptionPane.INFORMATION_MESSAGE);
     }
 }
--- src/ui/BottomPanel.java
+++ src/ui/BottomPanel.java
@@ -17,12 +17,7 @@ public class BottomPanel extends JPanel {
         JPanel controlPanel = new JPanel(new BorderLayout());
 
         JButton aboutButton = new JButton("Информация о разработчиках");
-        aboutButton.addActionListener(e -> {
-            JScrollPane scrollPane = new JScrollPane(new AboutDevsWindow());
-            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-            scrollPane.setPreferredSize(new Dimension(Math.min(screenSize.width, screenSize.height) - 100, screenSize.height - 200));
-            JOptionPane.showMessageDialog(application, scrollPane, "О разработчиках", JOptionPane.INFORMATION_MESSAGE);
-        });
+        aboutButton.addActionListener(e -> new AboutDevsWindow(application));
         controlPanel.add(aboutButton, BorderLayout.WEST);
 
         JSlider speedSlider = new JSlider(0, 100, 50);
