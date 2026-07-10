907d4e252abe83997371a8cd0f03a48af235efbd
PlestoPG <rogaliki.05@mail.ru>
2026-07-10 16:47:06 +0300

    Added font changer

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import java.awt.*;
 import javax.swing.*;
 import ui.Application;
+import ui.FontChanger;
 
 public class Main {
     public static void main(String[] args) {
+        FontChanger.apply(new Font("Gill Sans", Font.PLAIN, 16));
         SwingUtilities.invokeLater(Application::new);
     }
 }
